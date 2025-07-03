76252386e271bedcaf9017bb1161b1d96ce874cd
MOUNIKA YESULANKA <yesulankamounika@gmail.com>
2025-07-03 12:01:40 +0530

    Add files via upload

---
+++ listskip.py
@@ -0,0 +1,18 @@
+"""
+slicing is used to retriew the elements from one particular range to onother particular range
+the syntax:s^3(start:stop:skip)
+"""
+mylist=[1,2,13,14,15,16,17,18,19]
+print(mylist)
+print(mylist[0:6:2])
+
+mylist1 = [2,5,6,7,8,9,10]
+print(mylist1[0:7:3])
+
+
+mylist2 = [45,46,78,90,34,65,89]
+print(mylist2[0:8:4])
+
+
+mylist3 = ["hello",12,34,56,89.90,456+5j,455,789]
+print(mylist3[2:6:2])
