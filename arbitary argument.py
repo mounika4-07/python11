ba7e7278ee603b0a9202baa166edf870196f8cfc
MOUNIKA YESULANKA <yesulankamounika@gmail.com>
2025-07-15 14:51:39 +0530

    Add files via upload

---
+++ arbitary argument.py
@@ -0,0 +1,17 @@
+def function (*a):
+    print(a)
+function(10,15,20) 
+#arbitary argument is an argument by(*) which takes multiple arguments for a single parameter in the form of tuple
+#def function(a):
+  #  print(a)
+#function(10,15,20)
+def function(*a):
+    print(a)
+function(30,28,25)    
+#keyword argumentis a function which takes single parameter for multiple  in the form of keyvalue pairs so that the output is a 
+#dictionary format
+def function(**name):
+    print("hi",name)
+function(name="pandu",place="rjy")   
+
+
