a98bd37ca22ea6ebc0088b885c0e8ff881554258
MOUNIKA YESULANKA <yesulankamounika@gmail.com>
2025-07-15 14:50:24 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,42 @@
+#Function: Function is a block of code.it is excuted when it is called in phyton to create function
+#def keyword is used to function.def is nothing but different
+"""
+def function name(:)
+    statement/functionbody
+    function calling
+"""
+def mounika():#functiondefination
+    print("good morning")
+mounika()
+
+#note:the value that was passed into function defination is called parameters.
+#the value that was passed into function calling is called argument.
+#passing parameter and argument is a function.
+
+def function(a):#functiondefnation
+    print("this values",a)
+function(100)#functioncaling  
+
+#sinle parameter function:passing a single or one parameter function
+def function(c):#parameter 
+    print(c)
+function(143)  
+
+#multiple parameter functions:passing is a multiples values to a function is called as multiple parameters function
+def function3(a,b):
+    print("the value:",a+b)
+function3(40,28)  
+
+#perform a float division operation using function name flaot division operation
+def function(a,b):
+    print("the value:",a/b)
+function(5.6,7.78)    
+
+# write a program to wish a person using the python function
+def mounika():
+    print("HAPPY INDEPENDANCE DAY..")
+mounika() 
+
+# give a short note on python function operators
+
+
