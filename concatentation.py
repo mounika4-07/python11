3ca40915a95f872a9c5ad1e69c37c914072f860e
MOUNIKA YESULANKA <yesulankamounika@gmail.com>
2025-07-15 14:49:36 +0530

    Add files via upload

---
+++ concatentation.py
@@ -0,0 +1,17 @@
+#concatentation:(t):  this operator is used to concatentation the values.
+m=(1,2,3)
+n=(14,15,16)
+p=m+n
+print(p)
+# Repetation(*):this operator is repaet a given values.
+m=(1,2,3)
+n=m*3
+print(n)
+
+#len():this function used  to determine the len.
+s=(1,2,3)
+print(len(s))
+
+#count():this function used to count the no of repeated velues  in agiven squence
+b=(11,12,13,14,11,16,17,18)
+print(b.count(11))
